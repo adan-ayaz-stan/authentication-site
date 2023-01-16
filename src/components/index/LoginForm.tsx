b595e39b2360621e5d3300e835074de6c0142cc0
Adan <999a.d.a.n.a.y.a.z999@gmail.com>
2023-01-16 20:27:33 +0500

    Added Index Page Html

---
+++ src/components/index/LoginForm.tsx
@@ -0,0 +1,71 @@
+import { motion } from "framer-motion";
+
+import {
+  AiOutlineApple,
+  AiOutlineFacebook,
+  AiOutlineGooglePlus,
+} from "react-icons/ai/index";
+
+export default function LoginForm() {
+  return (
+    <motion.div
+      className="min-h-screen h-fit flex flex-col text-black justify-center px-8 lg:px-16 gap-4"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.5,
+        delay: 0.5,
+        type: "keyframes",
+        ease: "easeOut",
+      }}
+    >
+      <h1 className="text-3xl font-bold">Login</h1>
+      <h2 className="text-gray-700">Try out our amazing services.</h2>
+
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <label>Email Address</label>
+          <input type="text" className="p-3 bg-gray-200 rounded-xl" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label>Password</label>
+          <input type="text" className="p-3 bg-gray-200 rounded-xl" />
+        </div>
+
+        <a href="/" className="text-orange-500 font-bold hover:underline">
+          Forgot password
+        </a>
+
+        <button
+          type="submit"
+          className="p-3 text-black font-normal non-italic text-white bg-orange-500 rounded-xl uppercase"
+        >
+          Log In
+        </button>
+      </form>
+
+      <p className="w-full text-center">OR</p>
+
+      <div className="grid grid-cols-6 grid-rows-1 gap-4">
+        <a
+          href="/"
+          className="col-span-2 flex flex-row items-center justify-center p-2 text-white bg-[#af2121] rounded-xl"
+        >
+          <AiOutlineGooglePlus className="text-3xl" />
+        </a>
+        <a
+          href="/"
+          className="col-span-2 flex flex-row items-center justify-center p-2 text-white bg-[#3b5998] rounded-xl"
+        >
+          <AiOutlineFacebook className="text-3xl" />
+        </a>
+        <a
+          href="/"
+          className="col-span-2 flex flex-row items-center justify-center p-2 text-white bg-[#132133] hover:shadow-2xl rounded-xl"
+        >
+          <AiOutlineApple className="text-3xl" />
+        </a>
+      </div>
+    </motion.div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+import { motion } from "framer-motion";
 
-const inter = Inter({ subsets: ["latin"] });
+import LoginForm from "@/components/index/LoginForm";
+
+import logo from "../assets/svg/logo.png";
 
 export default function Home() {
   return (
@@ -14,27 +16,78 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="h-screen bg-white grid grid-cols-1 lg:grid-cols-2">
+      <main className="min-h-screen h-fit bg-white grid grid-cols-1 lg:grid-cols-2 lg:gap-0">
         {/* LEFT SIDE */}
-        <div
-          style={{
-            background:
-              "url('https://images.pexels.com/photos/11719086/pexels-photo-11719086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
-          }}
-          className="relative p-16 rounded-tr-[5em] rounded-br-[5em]"
-        >
-          <div className="h-full w-full flex flex-col justify-between p-12 bg-gray-500 bg-opacity-40 rounded-[3em]">
-            <p>Logo</p>
-            <div>
-              Welcome to We're a Digital Agency. We are glad to see you again!
-              Get access to your Orders, Wishlist and Recommendations.
+        <div className="min-h-screen relative p-3 lg:rounded-tr-[5em] lg:rounded-br-[5em] rounded-none text-center lg:text-left bg-[url('https://images.pexels.com/photos/11719086/pexels-photo-11719086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-no-repeat bg-cover">
+          <div className="h-full w-full flex flex-col justify-between p-12 text-black rounded-[3em]">
+            <motion.a
+              className="relative t-0 h-[120px] md:h-[200px]"
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.5,
+                delay: 0.5,
+                type: "keyframes",
+                ease: "easeOut",
+              }}
+            >
+              <Image
+                src={logo}
+                alt="logo"
+                fill={true}
+                className="m-auto object-cover"
+              />
+            </motion.a>
+            <div className="font-['Montserrat'] font-medium">
+              <motion.span
+                className="block text-3xl my-2 font-['Montserrat'] font-bold"
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.35,
+                  delay: 1,
+                  type: "keyframes",
+                  ease: "easeOut",
+                }}
+              >
+                Welcome to [Company]
+              </motion.span>
+              <motion.span
+                className="block"
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.35,
+                  delay: 1.3,
+                  type: "keyframes",
+                  ease: "easeOut",
+                }}
+              >
+                We're a Digital Agency. We are glad to see you again! <br /> Get
+                access to your Orders, Wishlist and Recommendations.
+              </motion.span>
             </div>
-            <div>
+            <motion.div
+              className="font-['Montserrat']"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{
+                duration: 0.35,
+                delay: 1.8,
+                type: "keyframes",
+                ease: "easeOut",
+              }}
+            >
               <p>Don't have an account?</p>
-              <a href="/">Register Now</a>
-            </div>
+              <a href="/" className="font-bold hover:underline">
+                Register Now
+              </a>
+            </motion.div>
           </div>
         </div>
+
+        {/* RIGHT SIDE */}
+        <LoginForm />
       </main>
     </>
   );
